13aeb1956aeacab954d5899e1dc78c851efa3646
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-11 20:14:29 +0800

    update

--- src/components/Sections/first.jsx
+++ src/components/Sections/first.jsx
@@ -3,29 +3,34 @@ import styled from "styled-components";
 // import QuotesIcon from "../../assets/svg/Quotes";
 import Dots from "../../assets/svg/Dots";
 import IconAndroid from "../../assets/img/android-screen.jpg";
+import IconiOS from "../../assets/img/ios.svg";
 
 export default function FirstPage() {
   return (
     <Wrapper id="home" className="container flexSpaceCenter">
       <LeftSide className="flexCenter">
         <div>
-          <h1 className="extraBold font40">Download SuperApp</h1>
-          <HeaderP className="font13 semiBold">
-            下载测试链接 下载测试链接 下载测试链接 下载测试链接 下载测试链接
-            下载测试链接 下载测试链接 下载测试链接 下载测试链接 下载测试链接
-            下载测试链接 下载测试链接
-          </HeaderP>
+          <h1 className="extraBold font40">Testing SuperApp</h1>
           <BtnWrapper>
-            <a
+            <AButton
               href="https://play.google.com/store/apps/details?id=com.seedao.app&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
                 alt="Get it on Google Play"
+                className="google"
                 src="https://play.google.com/intl/zh-TW/badges/static/images/badges/en_badge_web_generic.png"
               />
-            </a>
+            </AButton>
+            <AButton
+              src="https://testflight.apple.com/join/3QI4TSfd"
+              target="_blank"
+            >
+              <ButtonIcon src={IconiOS} alt="" />
+              <span>Download</span>
+              <div className="border"></div>
+            </AButton>
           </BtnWrapper>
         </div>
       </LeftSide>
@@ -49,7 +54,9 @@ export default function FirstPage() {
 
 const Wrapper = styled.section`
   padding-top: 20px;
-  width: 100%;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
   min-height: 840px;
   @media (max-width: 960px) {
     flex-direction: column;
@@ -58,6 +65,7 @@ const Wrapper = styled.section`
 const LeftSide = styled.div`
   width: 50%;
   height: 100%;
+  }
   @media (max-width: 960px) {
     width: 100%;
     order: 2;
@@ -77,19 +85,12 @@ const RightSide = styled.div`
     margin-top: 30px;
   }
 `;
-const HeaderP = styled.div`
-  max-width: 470px;
-  padding: 15px 0 50px 0;
-  line-height: 1.5rem;
-  @media (max-width: 960px) {
-    padding: 15px 0 50px 0;
-    text-align: center;
-    max-width: 100%;
-  }
-`;
 const BtnWrapper = styled.div`
-  max-width: 190px;
-  img {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-top: 50px;
+  img.google {
     width: 190px;
   }
   @media (max-width: 960px) {
@@ -144,4 +145,37 @@ const DotsWrapper = styled.div`
   @media (max-width: 560px) {
     display: none;
   }
+`;
+const AButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #232323;
+  gap: 10px;
+  width: 170px;
+  height: 54px;
+  border-radius: 8px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: 600;
+  position: relative;
+  span {
+    margin-top: 4px;
+  }
+  &:hover {
+    color: #fff;
+  }
+  .border {
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    width: calc(100% - 4px);
+    height: calc(100% - 4px);
+    border-radius: 8px;
+    border: 1px solid #c9c9c9;
+  }
+`;
+const ButtonIcon = styled.img`
+  width: 30px;
+  height: 30px;
 `;
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // Sections
-import Projects from "../components/Sections/Projects";
+// import Projects from "../components/Sections/Projects";
 import FirstPage from "../components/Sections/first";
 
 export default function Landing() {
   return (
     <>
       <FirstPage />
-      <Projects />
+      {/* <Projects /> */}
     </>
   );
 }
